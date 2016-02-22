fde3d755ff6c4e21fa99773ef84555650082e2e3
geekymoose <constantin.masson@etu.univ-paris-diderot.fr>
2016-02-22 22:01:50 +0100

    Update index.php to manage error

--- public/index.php
+++ public/index.php
@@ -3,9 +3,10 @@
  * Web site entry gate.
  * Load config etc and start render
  *
- * Creation Date: Feb 2016
- * Author: Constantin MASSON
+ * Since:	Date: Feb 2016
+ * Author:	Constantin MASSON
  */
+use \utils\database\DAOFactory;
 
 require './config.php';
 require '../utils/SplClassLoader.php';
@@ -17,8 +18,9 @@ $utilsLoader	->register();
 $modulesLoader	= new SplClassLoader('modules', __DIR__.'/..');
 $modulesLoader	->register();
 
-//Create router (To do before modules includes)
-$router = new utils\router\Router($_GET['url']);
+//Create router (To do before modules includes) and set Factory
+$router  = new utils\router\Router($_GET['url']);
+$factory = DAOFactory::getFactory(DAOFactory::PDO_DAO);
 
 //Scan modules dir and import each leaders
 $listFiles = scandir(PATH_MODULES);
@@ -28,24 +30,23 @@ foreach($listFiles as $file){
 	}
 }
 
-//@TODO DEBUG
-$lista = scandir(PATH_DATA.'imgs/');
-echo '<pre>';
-var_dump($lista);
-echo '</pre>';
-foreach($lista as $file){
-	if(is_file(PATH_DATA.'imgs/'.$file)){
-		echo $file;
-		echo '</br>';
-	}
-}
-
-//Start!!
+/*
+ * Start Router. Check if URL exists. 
+ * If exists, display, otherwise, display error route not found.
+ * If any other error occure, then error page displayed
+ */
 try{
 	$router->run();
 } catch (\utils\router\RouterException $ex){
 	$page = new \modules\core\models\Page();
 	$page->setContent(PATH_MODULES.'core/views/error404.phtml');
+	$page->addVar('errorMessage', "Unknown URL");
+	$page->renderPage();
+} catch (Exception $ex){
+	//@TODO Add log with error message (Something went wrong)
+	$page = new \modules\core\models\Page();
+	$page->setContent(PATH_MODULES.'core/views/error404.phtml');
+	$page->addVar('errorMessage', "Sorry, an error has occured");
 	$page->renderPage();
 }
 
